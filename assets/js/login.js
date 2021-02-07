9c2ec168129dcf4cca3dc91adce7e0ec200b0be6
= <sherefamaruf@gmail.com>
2021-02-07 17:12:44 +0300

    Did the login page

---
+++ assets/js/login.js
@@ -0,0 +1,24 @@
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = (() => {
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+});
+loginBtn.onclick = (() => {
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (() => {
+  signupBtn.click();
+  return false;
+});
+var backgrounds = ["b1","b2","b3","b4"];
+curIndex = 0;
+function changeBackground(){
+  document.querySelector("body").id = backgrounds[curIndex];
+  curIndex = curIndex < backgrounds.length-1 ? ++curIndex : 0;
+}
+setInterval(changeBackground,4000);
