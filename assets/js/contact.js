826e98a80ca5f3ab29608775280417ecc939e709
keiredin <se.keiredin.aman@gmail.com>
2021-02-15 16:12:12 +0300

    more js and css code added

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,12 +1,32 @@
-var submit_button=document.getElementById('submit');
-var inputs = document.querySelectorAll('input')
+
+var fullName = document.getElementById('name')
+var email = document.getElementById('mail')
 var message = document.getElementById('message')
 
+var submit_button=document.getElementById('submit');
+var forms = document.getElementsByTagName('form')
 
-submit_button.addEventListener("click", clearText);
 
+// submit_button.addEventListener("click", clearText);
+var mailForm = document.querySelector('.mailform');
+mailForm.addEventListener('submit',(e)=>{
+    fullName.value='';
+    email.value='';
+    message.value='';
 
-function clearText(){
-    inputs.forEach(input => input.value = "")
-    message.value="";
-}
+    // e.preventDefault();
+    // location.reload();
+})
+// function clearText(e){
+//     location.reload();
+//     // e.preventDefault();    
+//     // fullName.value='';
+//     // email.vale='';
+//     // message.value='';
+//     // fullName.submit();
+//     // email.submit();
+//     // message.submit();
+//     // email.reset();
+//     // email.reset();
+//     // message.reset();
+// }
